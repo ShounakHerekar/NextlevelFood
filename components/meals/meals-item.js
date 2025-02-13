f051e578887b85057995f6e2aff82f8d9d78eeda
Shounak Herekar <89743449+ShounakHerekar@users.noreply.github.com>
2025-02-13 16:08:42 +0530

    Update meals-item.js

--- components/meals/meals-item.js
+++ components/meals/meals-item.js
@@ -1,8 +1,20 @@
+"use client";
+
 import Link from 'next/link';
 import Image from 'next/image';
 import classes from './meals-item.module.css';
+import { useTransition } from 'react';
+import { removeMeal } from '../../lib/actions';
 
 export default function MealItem({ title, slug, image, summary, creator }) {
+  const [isPending, startTransition] = useTransition();
+
+  function handleDelete() {
+    startTransition(async () => {
+      await removeMeal(slug);
+    });
+  }
+
   return (
     <article className={classes.meal}>
       <header>
@@ -17,7 +29,16 @@ export default function MealItem({ title, slug, image, summary, creator }) {
       <div className={classes.content}>
         <p className={classes.summary}>{summary}</p>
         <div className={classes.actions}>
-          <Link href={`/meals/${slug}`}>View Details</Link>
+          <Link href={`/meals/${slug}`} className={classes.button}>
+            View Details
+          </Link>
+          <button 
+            onClick={handleDelete} 
+            disabled={isPending} 
+            className={classes.button} // Uses the same styling as the link
+          >
+            {isPending ? 'Deleting...' : 'Delete'}
+          </button>
         </div>
       </div>
     </article>
